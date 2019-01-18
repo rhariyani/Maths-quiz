90e64422163adc4724d598e2d0a46348c66a0dd3
rishita.hariyani@gmail.com <rishita.hariyani@gmail.com>
2019-01-17 20:48:05 -0700

    Pre Work Math Quiz

---
+++ Maths quiz/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.timeLabel = New System.Windows.Forms.Label()
+        Me.plusLeftLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.plusRightLabel = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Sum = New System.Windows.Forms.NumericUpDown()
+        Me.minusLeftLabel = New System.Windows.Forms.Label()
+        Me.minusRightLabel = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.difference = New System.Windows.Forms.NumericUpDown()
+        Me.dividedLeftLabel = New System.Windows.Forms.Label()
+        Me.timesRightLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.timeleft = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.product = New System.Windows.Forms.NumericUpDown()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.dividedRightLabel = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.quotient = New System.Windows.Forms.NumericUpDown()
+        Me.startButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.Sum, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.difference, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.product, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.quotient, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'timeLabel
+        '
+        Me.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.timeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.timeLabel.Location = New System.Drawing.Point(232, 18)
+        Me.timeLabel.Name = "timeLabel"
+        Me.timeLabel.Size = New System.Drawing.Size(200, 30)
+        Me.timeLabel.TabIndex = 0
+        '
+        'plusLeftLabel
+        '
+        Me.plusLeftLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.plusLeftLabel.Location = New System.Drawing.Point(8, 65)
+        Me.plusLeftLabel.Name = "plusLeftLabel"
+        Me.plusLeftLabel.Size = New System.Drawing.Size(60, 50)
+        Me.plusLeftLabel.TabIndex = 2
+        Me.plusLeftLabel.Text = "?"
+        Me.plusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(164, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 50)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "="
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'plusRightLabel
+        '
+        Me.plusRightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.plusRightLabel.Location = New System.Drawing.Point(111, 65)
+        Me.plusRightLabel.Name = "plusRightLabel"
+        Me.plusRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.plusRightLabel.TabIndex = 4
+        Me.plusRightLabel.Text = "?"
+        Me.plusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(63, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 50)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "+"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Sum
+        '
+        Me.Sum.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Sum.Location = New System.Drawing.Point(234, 73)
+        Me.Sum.Name = "Sum"
+        Me.Sum.Size = New System.Drawing.Size(100, 41)
+        Me.Sum.TabIndex = 2
+        '
+        'minusLeftLabel
+        '
+        Me.minusLeftLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minusLeftLabel.Location = New System.Drawing.Point(6, 127)
+        Me.minusLeftLabel.Name = "minusLeftLabel"
+        Me.minusLeftLabel.Size = New System.Drawing.Size(60, 50)
+        Me.minusLeftLabel.TabIndex = 7
+        Me.minusLeftLabel.Text = "?"
+        Me.minusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'minusRightLabel
+        '
+        Me.minusRightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minusRightLabel.Location = New System.Drawing.Point(111, 127)
+        Me.minusRightLabel.Name = "minusRightLabel"
+        Me.minusRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.minusRightLabel.TabIndex = 8
+        Me.minusRightLabel.Text = "?"
+        Me.minusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(63, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 50)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "-"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(167, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 50)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "="
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'difference
+        '
+        Me.difference.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.difference.Location = New System.Drawing.Point(232, 135)
+        Me.difference.Name = "difference"
+        Me.difference.Size = New System.Drawing.Size(100, 41)
+        Me.difference.TabIndex = 3
+        '
+        'dividedLeftLabel
+        '
+        Me.dividedLeftLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dividedLeftLabel.Location = New System.Drawing.Point(6, 248)
+        Me.dividedLeftLabel.Name = "dividedLeftLabel"
+        Me.dividedLeftLabel.Size = New System.Drawing.Size(60, 50)
+        Me.dividedLeftLabel.TabIndex = 12
+        Me.dividedLeftLabel.Text = "?"
+        Me.dividedLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'timesRightLabel
+        '
+        Me.timesRightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.timesRightLabel.Location = New System.Drawing.Point(109, 184)
+        Me.timesRightLabel.Name = "timesRightLabel"
+        Me.timesRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.timesRightLabel.TabIndex = 13
+        Me.timesRightLabel.Text = "?"
+        Me.timesRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(63, 186)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(60, 50)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "× "
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'timeleft
+        '
+        Me.timeleft.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.timeleft.Location = New System.Drawing.Point(12, 186)
+        Me.timeleft.Name = "timeleft"
+        Me.timeleft.Size = New System.Drawing.Size(60, 50)
+        Me.timeleft.TabIndex = 15
+        Me.timeleft.Text = "?"
+        Me.timeleft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(161, 182)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(60, 50)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "="
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'product
+        '
+        Me.product.Cursor = System.Windows.Forms.Cursors.Default
+        Me.product.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.product.Location = New System.Drawing.Point(232, 191)
+        Me.product.Name = "product"
+        Me.product.Size = New System.Drawing.Size(100, 41)
+        Me.product.TabIndex = 4
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(162, 240)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(60, 50)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "="
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'dividedRightLabel
+        '
+        Me.dividedRightLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dividedRightLabel.Location = New System.Drawing.Point(108, 244)
+        Me.dividedRightLabel.Name = "dividedRightLabel"
+        Me.dividedRightLabel.Size = New System.Drawing.Size(60, 50)
+        Me.dividedRightLabel.TabIndex = 19
+        Me.dividedRightLabel.Text = "?"
+        Me.dividedRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(64, 248)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(60, 50)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "÷ "
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'quotient
+        '
+        Me.quotient.Cursor = System.Windows.Forms.Cursors.Default
+        Me.quotient.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.quotient.Location = New System.Drawing.Point(234, 249)
+        Me.quotient.Name = "quotient"
+        Me.quotient.Size = New System.Drawing.Size(100, 41)
+        Me.quotient.TabIndex = 5
+        '
+        'startButton
+        '
+        Me.startButton.AutoSize = True
+        Me.startButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.startButton.Location = New System.Drawing.Point(100, 307)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(161, 39)
+        Me.startButton.TabIndex = 1
+        Me.startButton.Text = "Start the quiz"
+        Me.startButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 18)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(200, 30)
+        Me.Label5.TabIndex = 21
+        Me.Label5.Text = "TimeLeft"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 353)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.startButton)
+        Me.Controls.Add(Me.quotient)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.dividedRightLabel)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.product)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.timeleft)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.timesRightLabel)
+        Me.Controls.Add(Me.dividedLeftLabel)
+        Me.Controls.Add(Me.difference)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.minusRightLabel)
+        Me.Controls.Add(Me.minusLeftLabel)
+        Me.Controls.Add(Me.Sum)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.plusRightLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.plusLeftLabel)
+        Me.Controls.Add(Me.timeLabel)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Math quiz"
+        CType(Me.Sum, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.difference, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.product, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.quotient, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents timeLabel As Label
+    Friend WithEvents timeleft As Label
+    Friend WithEvents plusLeftLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents plusRightLabel As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Sum As NumericUpDown
+    Friend WithEvents minusLeftLabel As Label
+    Friend WithEvents minusRightLabel As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents difference As NumericUpDown
+    Friend WithEvents dividedLeftLabel As Label
+    Friend WithEvents timesRightLabel As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents product As NumericUpDown
+    Friend WithEvents Label8 As Label
+    Friend WithEvents dividedRightLabel As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents quotient As NumericUpDown
+    Friend WithEvents startButton As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Maths quiz/Form1.vb
@@ -0,0 +1,210 @@
+﻿Public Class Form1
+    Private Const V1 As String = " seconds"
+    Private Const V2 As Integer = 30
+
+    ' Create a Random object called randomizer 
+    ' to generate random numbers.
+    Private randomizer As New Random
+
+    ' These integer variables store the numbers 
+    ' for the addition problem. 
+    Private addend1 As Integer
+    Private addend2 As Integer
+
+    ' These integer variables store the numbers 
+    ' for the subtraction problem. 
+    Private minuend As Integer
+    Private subtrahend As Integer
+
+    'These integer variables store the numbers 
+    ' for the multiplication problem. 
+    Private multiplicand As Integer
+    Private multiplier As Integer
+
+    ' These integer variables store the numbers 
+    ' for the division problem. 
+    Private dividend As Integer
+    Private divisor As Integer
+
+    ' This integer variable keeps track of the 
+    ' remaining time.
+    Private timesleft As Integer
+
+    ''' <summary>
+    ''' Start the quiz by filling in all of the problem 
+    ''' values and starting the timer. 
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub StartTheQuiz()
+
+        ' Fill in the addition problem.
+        ' Generate two random numbers to add.
+        ' Store the values in the variables 'addend1' and 'addend2'.
+        addend1 = randomizer.Next(51)
+        addend2 = randomizer.Next(51)
+
+        ' Convert the two randomly generated numbers
+        ' into strings so that they can be displayed
+        ' in the label controls.
+        plusLeftLabel.Text = addend1.ToString()
+        plusRightLabel.Text = addend2.ToString()
+
+        ' 'sum' is the name of the NumericUpDown control.
+        ' This step makes sure its value is zero before
+        ' adding any values to it.
+        Sum.Value = 0
+
+
+        ' Fill in the subtraction problem.
+        minuend = randomizer.Next(1, 101)
+        subtrahend = randomizer.Next(1, minuend)
+        minusLeftLabel.Text = minuend.ToString()
+        minusRightLabel.Text = subtrahend.ToString()
+        difference.Value = 0
+
+        ' Fill in the multiplication problem.
+        multiplicand = randomizer.Next(2, 11)
+        multiplier = randomizer.Next(2, 11)
+        timeleft.Text = multiplicand.ToString()
+        timesRightLabel.Text = multiplier.ToString()
+        product.Value = 0
+
+        ' Fill in the division problem.
+        divisor = randomizer.Next(2, 11)
+        Dim temporaryQuotient As Integer = randomizer.Next(2, 11)
+        dividend = divisor * temporaryQuotient
+        dividedLeftLabel.Text = dividend.ToString()
+        dividedRightLabel.Text = divisor.ToString()
+        quotient.Value = 0
+
+
+        ' Start the timer.
+
+        timeLabel.Text = "30 seconds"
+        Timer1.Start()
+
+    End Sub
+
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles difference.ValueChanged
+
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged_1(sender As Object, e As EventArgs) Handles product.ValueChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles startButton.Click
+        ' Call the StartTheQuiz() method and enable
+        ' the Start button. 
+
+        StartTheQuiz()
+        startButton.Enabled = False
+
+
+
+    End Sub
+    ''' <summary>
+    ''' Check the answers to see if the user got everything right.
+    ''' </summary>
+    ''' <returns>True if the answer's correct, false otherwise.</returns>
+    ''' <remarks></remarks>
+    Public Function CheckTheAnswer() As Boolean
+
+        If addend1 + addend2 = Sum.Value AndAlso
+       minuend - subtrahend = difference.Value AndAlso
+       multiplicand * multiplier = product.Value AndAlso
+       dividend / divisor = quotient.Value Then
+
+
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+    ''' <summary> 
+    ''' Modify the behavior of the NumericUpDown control
+    ''' to make it easier to enter numeric values for
+    ''' the quiz.
+    ''' </summary> 
+    Private Sub answer_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Sum.Enter, difference.Enter, product.Enter, quotient.Enter
+
+        ' Select the whole answer in the NumericUpDown control.
+        Dim answerBox = TryCast(sender, NumericUpDown)
+
+        If answerBox IsNot Nothing Then
+            Dim lengthOfAnswer = answerBox.Value.ToString().Length
+            answerBox.Select(0, lengthOfAnswer)
+        End If
+
+    End Sub
+
+
+    Private Sub Timer1_Tick() Handles Timer1.Tick
+
+        If timesleft <= 0 Then
+            ' If the user ran out of time, stop the timer, show
+            ' a MessageBox, and fill in the answers.
+            Timer1.Stop()
+            timeLabel.Text = "Time's up!"
+            MessageBox.Show("You didn't finish in time.", "Sorry!")
+            Sum.Value = addend1 + addend2
+            startButton.Enabled = True
+        Else
+            ' Display the new time left
+            ' by updating the Time Left label.
+            timesleft -= 1
+            Dim timesleft1 As Object = timesleft
+            timeLabel.Text = timesleft1 & " seconds"
+
+
+            ' If the user ran out of time, stop the timer, show 
+            ' a MessageBox, and fill in the answers.
+            Timer1.Stop()
+            timeLabel.Text = "Time's up!"
+            MessageBox.Show("You didn't finish in time.", "Sorry!")
+            Sum.Value = addend1 + addend2
+            startButton.Enabled = True
+        End If
+
+
+        If CheckTheAnswer() Then
+            ' If CheckTheAnswer() returns true, then the user 
+            ' got the answer right. Stop the timer  
+            ' and show a MessageBox.
+            Timer1.Stop()
+            MessageBox.Show("You got all of the answers right!", "Congratulations!")
+            startButton.Enabled = True
+        ElseIf timesleft > 0 Then
+            ' If CheckTheAnswer() return false, keep counting
+            ' down. Decrease the time left by one second and 
+            ' display the new time left by updating the 
+            ' Time Left label.
+            timesleft -= 1
+            timeLabel.Text = timesleft & " seconds"
+        Else
+            ' If the user ran out of time, stop the timer, show 
+            ' a MessageBox, and fill in the answers.
+            Timer1.Stop()
+            timeLabel.Text = "Time's up!"
+            MessageBox.Show("You didn't finish in time.", "Sorry!")
+            Sum.Value = addend1 + addend2
+            difference.Value = minuend - subtrahend
+            startButton.Enabled = True
+            product.Value = multiplicand * multiplier
+            quotient.Value = dividend / divisor
+        End If
+
+
+    End Sub
+
+End Class
